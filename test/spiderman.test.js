aae213253a24bbaaa7b7aae11fa23839019c064d
Luis Armando Prado <armap1999@hotmail.com>
2022-04-21 12:14:40 -0500

    feat: second unit test

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -10,4 +10,10 @@ describe("Unit test for Spiderman class", () => {
         expect(spiderman.movies).toBe(2);
         expect(spiderman.studio).toBe("Sony");
      })
+
+     test('2) Use getInfo method', () => { 
+        const spiderman = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony");
+
+        expect(spiderman.getInfo()).toBe("Hey, I'm Spiderman Sony from Sony");
+     })
 });
